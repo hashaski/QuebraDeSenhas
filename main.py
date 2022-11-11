0b69714de2f36e0540e116ad956ed230d711ee72
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2022-11-11 14:57:36 -0300

    Changed 'gerar_combinacoes' so that combinations are generated in ascending order (by word number).

--- main.py
+++ main.py
@@ -18,17 +18,19 @@ def codificar_senha(senha):
     return digest_b64_encoded_utf8_decoded
 
 
-def gerar_combinacoes(lista, tamanho_max):
+def gerar_combinacoes(lista, tamanho_max, tamanho = 1):
     """
     Gera todas as combinações possíveis dos elementos da lista 'lista',
-    de tamanho 'tamanho_max' até 1.
+    de tamanho 'tamanho_max' até 1, e as insere na lista global 'combinacoes'.
     """
     global combinacoes
-    combinacoes.extend(itertools.combinations_with_replacement(lista, tamanho_max))
-    if tamanho_max > 1:
-        gerar_combinacoes(lista, tamanho_max - 1)
+    combinacoes.extend(itertools.combinations_with_replacement(lista, tamanho))
+    #debug_combinacoes.write(f"{list(itertools.combinations_with_replacement(lista, tamanho))}\n")
+    if tamanho < tamanho_max:
+        gerar_combinacoes(lista, tamanho_max, tamanho + 1)
     else:
         return
+
 
 def verificar(combinacao):
     """
@@ -53,6 +55,7 @@ def verificar(combinacao):
 #print('#Senha Codificada:', codificada)
 
 palavras_file = open("palavras.txt", "r")
+debug_combinacoes = open("debug_combinacoes.txt", "w")
 credenciais_codificadas_file = open("usuarios_senhascodificadas.txt", "r")
 senhas_quebradas_file = open("senhas_quebradas.txt", "w")
 senhas_nao_quebradas_file = open("senhas_nao_quebradas.txt", "w")
@@ -71,5 +74,3 @@ gerar_combinacoes(palavras, 5)
 
 for combinacao in combinacoes:
     verificar(" ".join(combinacao))
-
-# teste
